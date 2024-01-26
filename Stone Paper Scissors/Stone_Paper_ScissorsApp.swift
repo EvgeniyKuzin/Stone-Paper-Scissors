c155eaa0c070ced2bb15d4f78bfd8be3a59e5c74
EvgeniyKuzin <vaglover@yandex.ru>
2024-01-26 18:34:39 +0300

    Initial Commit

---
+++ Stone Paper Scissors/Stone_Paper_ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stone_Paper_ScissorsApp.swift
+//  Stone Paper Scissors
+//
+//  Created by Evgeniy Kuzin on 26.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Stone_Paper_ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
